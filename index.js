2aadb079047eaaaed4e7a7b30654625fe8f3c441
jindada <jzs0914@126.com>
2019-06-09 09:52:18 +0800

    fix: delete BackAndroid

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ var {
   TouchableWithoutFeedback,
   Dimensions,
   Easing,
-  BackAndroid,
   BackHandler,
   Platform,
   Modal,
@@ -20,7 +19,7 @@ var {
 
 var createReactClass = require('create-react-class');
 
-var BackButton = BackHandler || BackAndroid;
+var BackButton = BackHandler;
 
 var screen = Dimensions.get('window');
 
